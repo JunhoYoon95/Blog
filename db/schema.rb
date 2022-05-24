4bb40c889eb2e47d67a326aed77e4753363a8178
JunhoYoon95 <enffl95@icloud.com>
2022-05-24 17:59:19 +0900

    부트스트랩을 들여와서 Header 을 만들어서 두었습니다만

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_24_031919) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_24_081615) do
+  create_table "posts", charset: "utf8mb3", collation: "utf8_bin", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb3", collation: "utf8_bin", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
